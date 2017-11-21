0fca5fe130254f5898d534da0b55751874f9cdcf
David Callizaya <davidcallizaya@172.16.20.75>
2017-11-21 06:36:23 -0400

    add fields

--- app/Lead.php
+++ app/Lead.php
@@ -17,6 +17,17 @@ class Lead
     const PHONE = 'phone_mobile';
     const CITY = 'crm_city_c';
     const LANDING_CODE = 'crm_landing_code_c';
+    const PRIMER_NOMBRE= 'crm_primer_nombre_c';
+    const SEGUNDO_NOMBRE= 'crm_segundo_nombre_c';
+    const APELLIDO_PATERNO= 'crm_apellido_paterno_c';
+    const APELLIDO_MATERNO= 'crm_apellido_materno_c';
+    const APELLIDO_CASADA= 'crm_apellido_casada_c';
+    const TIPO_DOCUMENTO= 'crm_tipo_documento_c';
+    const NRO_DOCUMENTO= 'crm_nro_documento_c';
+    const EXTENSION= 'crm_extension_c';
+    const CC_CIUDAD= 'cc_ciudad_c';
+    const CC_AGENCIA= 'cc_agencia_c';
+    const CC_USUARIO= 'cc_usuario_c';
 
     private static $alias = [
         'variant' => 'crm_variant_c',
@@ -75,6 +86,17 @@ class Lead
                     self::AMOUNT,
                     'date_entered',
                     self::LANDING_CODE,
+                    self::PRIMER_NOMBRE,
+                    self::SEGUNDO_NOMBRE,
+                    self::APELLIDO_PATERNO,
+                    self::APELLIDO_MATERNO,
+                    self::APELLIDO_CASADA,
+                    self::TIPO_DOCUMENTO,
+                    self::NRO_DOCUMENTO,
+                    self::EXTENSION,
+                    self::CC_CIUDAD,
+                    self::CC_AGENCIA,
+                    self::CC_USUARIO,
                 ],
                 [
                     'where' => $where
