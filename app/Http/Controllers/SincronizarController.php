7d28347fa371ac448e630796fda36fb73d443e5f
David Callizaya <davidcallizaya@gmail.com>
2018-10-10 17:18:34 -0400

    ttl

--- app/Http/Controllers/SincronizarController.php
+++ app/Http/Controllers/SincronizarController.php
@@ -23,8 +23,12 @@ class SincronizarController extends Controller
             return;
         }
         $response = [];
+        $ttl = 10;
         foreach ($res as $re) {
             $response[] = $this->procesar($re);
+            $ttl--;
+            if (!$ttl)
+                break;
         }
         return view('sync', ['response' => $response]);
     }
